0ad38cd8cfb834726473e75359110dbb9a41c43e
khaledtag <khaled1544_1@yahoo.com>
2020-03-25 00:03:35 +0200

    Update dashboard.js

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,12 +1,12 @@
-function ProgressCalc() {
-    let NumOfEpisodes = 20;
-    let seen = 10
-    let progress = (seen / NumOfEpisodes) * 100;
-    //for (let i =1 ; i<=2 ; i++);
-    let prog1 = document.getElementById("prog-bar1")
-    prog1.style.setProperty("--width", progress)
-    // let prog2 = document.getElementById("prog-bar2")
-    // prog2.style.setProperty("--width", progress)
-}
+// function ProgressCalc() {
+//     let NumOfEpisodes = 20;
+//     let seen = 10
+//     let progress = (seen / NumOfEpisodes) * 100;
+//     //for (let i =1 ; i<=2 ; i++);
+//     let prog1 = document.getElementById("prog-bar1")
+//     prog1.style.setProperty("--width", progress)
+//     // let prog2 = document.getElementById("prog-bar2")
+//     // prog2.style.setProperty("--width", progress)
+// }
 
-ProgressCalc();
+// ProgressCalc();
