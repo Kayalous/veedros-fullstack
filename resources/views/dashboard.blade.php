525362ec8aa15937b260f42fcbdcf10f96464523
khaledtag <khaled1544_1@yahoo.com>
2020-03-25 04:17:45 +0200

    route added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,6 @@
 @endsection
 
 @section('content')
-<div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v6.0"></script>
     <div class="new-bg"></div>
     <section class="info-bar">
         <div class="container ">
