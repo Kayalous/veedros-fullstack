e9326beafc10dca68794b91ba72ac0107fa9b1ab
khaledtag <khaled1544_1@yahoo.com>
2020-03-26 00:15:42 +0200

    add overlay form sec

--- resources/views/teach.blade.php
+++ resources/views/teach.blade.php
@@ -86,6 +86,7 @@
     <section class="my-5 teach-form">
         <div class="row">
             <div class="col-12 form-bg col-lg-6">
+                <div class="form-overlay"></div>
                 <div class="form-content w-100 m-5">
                     <ul>
                         <li>
