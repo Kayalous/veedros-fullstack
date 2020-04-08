f130d33e4c36d9d9dbb4b777fe49fa23a6ea138b
Kayalous <52806207+Kayalous@users.noreply.github.com>
2020-04-08 18:24:39 +0200

    Created cronjob to delete passwordless login entries older than 1 hour

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -3,7 +3,7 @@
 
 Please click on this link to login now: <a href="{{$url}}">{{$url}}</a>
 
-if you didn't try to login don't worry, you don't have to do anything. The link will expire in 15 minutes.
+if you didn't try to login don't worry, you don't have to do anything. The link will expire in 60 minutes.
 
 <br>
 If you're having trouble logging in please contact the support at support@veedros.com.
