8412a7b9234a5c319adc35cc3860bf6997d67ecc
khaledtag <khaled1544_1@yahoo.com>
2020-05-06 21:55:23 +0200

    Update route.blade.php

--- resources/views/route.blade.php
+++ resources/views/route.blade.php
@@ -11,15 +11,9 @@
             <h1>Welcome to Route</h1>
             <img src="{{asset('images')}}/Route_logo.png" alt="">
             <div class="mt-3">
-            <div class="btn-redeem-veedros m-auto ">
-            <form method="POST" action="/promo/redeem">
-                <input type="hidden" name="_token" value="ptBO1JyGWqEb9ZGu0SY7hGSFpLRKvgDnl3WcQbKV">
-                <input class="btn-redeem form-control profile-form-field email-field-props border-light border-radius-sm" placeholder="Enter cycle passcode..." type="text" name="code">
-                <button type="submit" class="btn-redeem-icon border-0">
-                    <i class="fas fa-arrow-right" aria-hidden="true"></i>
-                </button>
-            </form>
-        </div>
+            <a href="" class="add-cycle" data-toggle="modal" data-target="#exampleModalCenter">
+                <i class="fas fa-plus"></i>
+            </a>
 
             </div>
             
