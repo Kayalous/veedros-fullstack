20f76e77dd244990fe0754d4298a1822fd8f9104
khaledtag <khaled1544_1@yahoo.com>
2020-05-06 17:39:17 +0200

    join cycle

--- resources/views/route.blade.php
+++ resources/views/route.blade.php
@@ -10,10 +10,23 @@
         <div class="content">
             <h1>Welcome to Route</h1>
             <img src="{{asset('images')}}/Route_logo.png" alt="">
+            <div class="mt-3">
+            <div class="btn-redeem-veedros m-auto ">
+            <form method="POST" action="/promo/redeem">
+                <input type="hidden" name="_token" value="ptBO1JyGWqEb9ZGu0SY7hGSFpLRKvgDnl3WcQbKV">
+                <input class="btn-redeem form-control profile-form-field email-field-props border-light border-radius-sm" placeholder="Enter cycle passcode..." type="text" name="code">
+                <button type="submit" class="btn-redeem-icon border-0">
+                    <i class="fas fa-arrow-right" aria-hidden="true"></i>
+                </button>
+            </form>
+        </div>
+
+            </div>
+            
         </div>
     </div>
 
-    <section class="avalible-sessions my-5">
+    <!-- <section class="avalible-sessions my-5">
     <div class="container">
         <div class="row">
             <div class="header">
@@ -24,7 +37,7 @@
             </div>
         </div>
 
-        <!-- <div class="row justify-content-center">
+        <div class="row justify-content-center">
             <div class="col-12 col-md-5 col-lg-3">
                 <div class="card course-card development-card noJquery" style="background-color: #A7A7A7"
                     data-toggle="modal" data-target="#exampleModal">
@@ -62,8 +75,37 @@
         </div> -->
 
       
+    <a href="" class="add-cycle" data-toggle="modal" data-target="#exampleModalCenter">
+    <i class="fas fa-plus"></i>
+    </a>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Join cycle</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="btn-redeem-veedros m-auto ">
+            <form method="POST" action="/promo/redeem">
+                <input type="hidden" name="_token" value="ptBO1JyGWqEb9ZGu0SY7hGSFpLRKvgDnl3WcQbKV">
+                <input class="btn-redeem form-control profile-form-field email-field-props border-light border-radius-sm" placeholder="Enter cycle passcode..." type="text" name="code">
+                <button type="submit" class="btn-redeem-icon border-0">
+                    <i class="fas fa-arrow-right" aria-hidden="true"></i>
+                </button>
+            </form>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
     </div>
+  </div>
+</div>
 </section>
 @endsection
 
